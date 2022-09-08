e67b4d12a97a7b91044f814988b029e93a5da48c
Sosthenes Oliveira Lima <sosthenes.lima@gmail.com>
2022-09-07 23:30:28 -0300

    commit inicial

---
+++ TaskController.java
@@ -0,0 +1,245 @@
+
+package controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Task;
+import util.ConnectionFactory;
+
+/**
+ *
+ * @author User
+ */
+public class TaskController {
+    
+    
+    public void save(Task task) {
+        String sql = "INSERT INTO tasks(idProject, name, description, completed, notes, deadline, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        
+         try {
+            //Cria uma conex?o com o banco
+            conn = ConnectionFactory.getConnection();
+            //Cria um PreparedStatment, classe usada para executar a query
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, task.getIdProject());
+            stmt.setString(2, task.getName());
+            stmt.setString(3, task.getDescription());
+            stmt.setBoolean(4, task.isCompleted());
+            stmt.setString(5, task.getNotes());
+            stmt.setDate(6, new java.sql.Date(task.getDeadline().getTime()));
+            stmt.setDate(7, new java.sql.Date(task.getCreatedAt().getTime()));
+            stmt.setDate(8, new java.sql.Date(task.getUpdatedAt().getTime()));
+            //Executa a sql para inser??o dos dados
+            stmt.execute();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao salvar a tarefa " + ex.getMessage(), ex);
+        } finally {
+            //Fecha as conex?es
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro ao fechar a conexão" + ex.getMessage(), ex);
+            }
+        }
+
+    }
+
+    public void update(Task task) {
+
+        String sql = "UPDATE tasks SET idProject = ?, name = ?, description = ?,  completed = ?, notes = ?, deadline = ?, createdAt = ?, updatedAt = ? WHERE id = ?";
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            //Cria uma conex?o com o banco
+            conn = ConnectionFactory.getConnection();
+            //Cria um PreparedStatment, classe usada para executar a query
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, task.getIdProject());
+            stmt.setString(2, task.getName());
+            stmt.setString(3, task.getDescription());
+            stmt.setBoolean(4, task.isCompleted());
+            stmt.setString(5, task.getNotes());
+            stmt.setDate(6, new java.sql.Date(task.getDeadline().getTime()));
+            stmt.setDate(7, new java.sql.Date(task.getCreatedAt().getTime()));
+            stmt.setDate(8, new java.sql.Date(task.getUpdatedAt().getTime()));
+            stmt.setInt(9, task.getId());
+
+            //Executa a sql para inser??o dos dados
+            stmt.execute();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro em atualizar a tarefa" + ex.getMessage(), ex);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro ao fechar a conexão" + ex.getMessage(), ex);
+            }
+        }
+    }
+
+    public List<Task> getAll() {
+        String sql = "SELECT * FROM tasks";
+
+        List<Task> tasks = new ArrayList<>();
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        //Classe que vai recuperar os dados do banco de dados
+        ResultSet rset = null;
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            stmt = conn.prepareStatement(sql);
+
+            rset = stmt.executeQuery();
+
+            //Enquanto existir dados no banco de dados, fa?a
+            while (rset.next()) {
+
+                Task task = new Task();
+
+                task.setId(rset.getInt("id"));
+                task.setIdProject(rset.getInt("idProject"));
+                task.setName(rset.getString("name"));
+                task.setDescription(rset.getString("description"));
+                task.setCompleted(rset.getBoolean("completed"));
+                task.setNotes(rset.getString("notes"));
+                task.setDeadline(rset.getDate("deadline"));
+                task.setCreatedAt(rset.getDate("createdAt"));
+                task.setCreatedAt(rset.getDate("updatedAt"));
+
+                //Adiciono o contato recuperado, a lista de contatos
+                tasks.add(task);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao buscar as tarefas" + ex.getMessage(), ex);
+        } finally {
+            try {
+                if (rset != null) {
+                    rset.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro ao fechar a conexão" + ex.getMessage(), ex);
+            }
+        }
+        return tasks;
+    }
+
+    public List<Task> getByProjectId(int id) {
+        String sql = "SELECT * FROM tasks where idProject = ?";
+
+        List<Task> tasks = new ArrayList<>();
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        //Classe que vai recuperar os dados do banco de dados
+        ResultSet rset = null;
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, id);
+
+            rset = stmt.executeQuery();
+
+            //Enquanto existir dados no banco de dados, fa?a
+            while (rset.next()) {
+
+                Task task = new Task();
+
+                task.setId(rset.getInt("id"));
+                task.setIdProject(rset.getInt("idProject"));
+                task.setName(rset.getString("name"));
+                task.setDescription(rset.getString("description"));
+                task.setCompleted(rset.getBoolean("completed"));
+                task.setNotes(rset.getString("notes"));
+                task.setDeadline(rset.getDate("deadline"));
+                task.setCreatedAt(rset.getDate("createdAt"));
+                task.setCreatedAt(rset.getDate("updatedAt"));
+
+                //Adiciono o contato recuperado, a lista de contatos
+                tasks.add(task);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao buscar as tarefas" + ex.getMessage(), ex);
+        } finally {
+            try {
+                if (rset != null) {
+                    rset.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro ao fechar a conexão" + ex.getMessage(), ex);
+            }
+        }
+        return tasks;
+    }
+
+    public void removeById(int taskId) {
+
+        String sql = "DELETE FROM tasks WHERE id = ?";
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, taskId);
+            stmt.execute();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao deletar a tarefa" + ex.getMessage(), ex);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro ao fechar a conexão" + ex.getMessage(), ex);
+            }
+        }
+
+    }
+
+ }    
+    
+    
+
+
